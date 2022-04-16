f523a423175601662899e8bc5fd7f8de6a9e6765
Ahmad Hisyam <hisyam@Ahmads-MacBook-Air.local>
2022-04-16 22:45:46 +0700

    add testing unit, cypress

---
+++ cypress/integration/dashboard/Dashboard.spec.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe('Dashboard page', () =>{
+    beforeEach(()=>{
+        cy.visit("/dashboard")
+    })
+
+    it("display title page Dashboard",()=>{
+        cy.get('title').contains("Dashboard")
+    })
+
+})
